565d2da226762249f61a97350438344be5ae4d72
Md-Sxmeer <zm24adu@herts.ac.uk>
2024-11-24 23:19:38 +0000

    added counts module to the code

--- RProject.R
+++ RProject.R
@@ -3,3 +3,9 @@ data <- read.csv("Match.csv")
 
 # Remove rows with missing values in the Match_Winner column
 data <- na.omit(data)
+
+# Calculate the counts for each category
+toss_winner_win <- sum(data$Toss_Winner_Match_Winner == TRUE)
+toss_winner_loss <- sum(data$Toss_Winner_Match_Winner == FALSE & data$Toss_Winner != data$Match_Winner) # Toss Winner loses
+toss_loser_win <- sum(data$Toss_Winner_Match_Winner == FALSE & data$Toss_Winner != data$Match_Winner) # Toss Loser wins
+toss_loser_loss <- sum(data$Toss_Winner_Match_Winner == FALSE & data$Toss_Winner == data$Match_Winner) # Toss Loser loses
