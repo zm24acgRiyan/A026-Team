f0e37683df125ac210220c9a543ec9cf839e11d3
ac007a <ac24ado@herts.ac.uk>
2024-11-24 23:38:14 +0000

    added number of matches and there percentage

--- RProject.R
+++ RProject.R
@@ -9,3 +9,14 @@ toss_winner_win <- sum(data$Toss_Winner_Match_Winner == TRUE)
 toss_winner_loss <- sum(data$Toss_Winner_Match_Winner == FALSE & data$Toss_Winner != data$Match_Winner) # Toss Winner loses
 toss_loser_win <- sum(data$Toss_Winner_Match_Winner == FALSE & data$Toss_Winner != data$Match_Winner) # Toss Loser wins
 toss_loser_loss <- sum(data$Toss_Winner_Match_Winner == FALSE & data$Toss_Winner == data$Match_Winner) # Toss Loser loses
+
+# Total number of matches
+total_matches <- nrow(data)
+
+# Calculate the percentage for each category
+toss_winner_win_percentage <- (toss_winner_win / total_matches) * 100
+toss_winner_loss_percentage <- (toss_winner_loss / total_matches) * 100
+toss_loser_win_percentage <- (toss_loser_win / total_matches) * 100
+toss_loser_loss_percentage <- (toss_loser_loss / total_matches) * 100.
+
+
